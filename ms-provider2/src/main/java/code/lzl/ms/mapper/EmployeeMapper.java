efff7ae10d6576a1b00d9c135cd9ab2cd5d72fec
snownnlee <snownnlee@163.com>
2020-05-31 14:12:13 +0800

    添加服务提供者provider2

---
+++ ms-provider2/src/main/java/code/lzl/ms/mapper/EmployeeMapper.java
@@ -0,0 +1,9 @@
+package code.lzl.ms.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+import code.lzl.ms.entity.Employee;
+
+public interface EmployeeMapper extends BaseMapper<Employee> {
+
+}
