02b3ac087bbd8dd1e648b65129f5de0138e09915
snownnlee <snownnlee@163.com>
2020-06-01 01:06:42 +0800

    单独使用Hystrix【ms-consumer】

--- ms-consumer/src/main/java/code/lzl/ms/ConsumerApplication.java
+++ ms-consumer/src/main/java/code/lzl/ms/ConsumerApplication.java
@@ -2,10 +2,12 @@ package code.lzl.ms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableCircuitBreaker
 public class ConsumerApplication {
 	
 	public static void main(String[] args) throws Exception {
--- ms-consumer/src/main/java/code/lzl/ms/controller/IndexController.java
+++ ms-consumer/src/main/java/code/lzl/ms/controller/IndexController.java
@@ -1,9 +1,13 @@
 package code.lzl.ms.controller;
 
+import java.util.Random;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class IndexController {
@@ -11,6 +15,18 @@ public class IndexController {
 	@GetMapping("/")
 	public String find() {
 		return "Hello, I am Consumer!";
+	}
+	
+	private Random random = new Random();
+	
+	@GetMapping("/test/hystrix")
+	@HystrixCommand(fallbackMethod = "testHystrixFallback")
+	public String testHystrix() {
+		return String.valueOf(2/random.nextInt(2));
+	}
+	
+	public String testHystrixFallback() {
+		return "Err:【I am testHystrixFallback!】";
 	}
 	
 	@Autowired
