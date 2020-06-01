52ea0daea763303ab19cdb608c2a66274072432f
snownnlee <snownnlee@163.com>
2020-06-01 21:37:59 +0800

    #由于eureka-client模块的自动配置，可以去掉@EnableEurekaClient注解

--- ms-consumer-feign/src/main/java/code/lzl/ms/ConsumerFeignApplication.java
+++ ms-consumer-feign/src/main/java/code/lzl/ms/ConsumerFeignApplication.java
@@ -3,11 +3,9 @@ package code.lzl.ms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableEurekaClient
 @EnableFeignClients
 @EnableCircuitBreaker
 public class ConsumerFeignApplication {
